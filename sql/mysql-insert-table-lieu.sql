2d63f4cafd87b0c81123d38e9ba019fd9f9a84bb
gbosznai <gbosznai@btssio-VirtualBox>
2017-10-19 15:48:34 +0200

    Creation des scripts de création de la table Lieu et d'insertion des valeurs dans la table Lieu

---
+++ sql/mysql-insert-table-lieu.sql
@@ -0,0 +1,6 @@
+--Insertion des valeurs dans la table Lieu
+
+INSERT INTO Lieu VALUES (1, 'SALLE DU PANIER FLEURI', 'Rue de Bonneville', 450);
+INSERT INTO Lieu VALUES (2, 'LE CABARET', 'MAIRIE ANNEXE DE PARAME, Place Georges COUDRAY', 250);
+INSERT INTO Lieu VALUES (3, 'LE PARC DES CHENES', '14 rue des chênes', 2000);
+INSERT INTO Lieu VALUES (4, 'LE VILLAGE', 'Ecole LEGATELOIS, 25 rue Général de Castelnau', 500);
